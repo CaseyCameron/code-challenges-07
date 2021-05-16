c3485254ae70932defb9bcefe184453918fd1d5f
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-16 10:43:24 -0700

    can't figure out validatePhoneNumber

---
+++ src/ch-14.js
@@ -0,0 +1,23 @@
+export function replaceZeros(str){
+  return str.replace(/[0]/ig, 'zero');
+}
+
+export function validatePin(str){
+  return /^[0-9]{4}$/.test(str);
+}
+
+export function validateWord(str){
+  return /^[A-Za-z]{5,10}$/.test(str);
+}
+
+export function hasNumber(str){
+  return (/[A-Za-z]+(\d+)$/).test(str);
+}
+
+export function validateEmail(str){
+  return /^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+(?:\.com$|.net$|.org$)/.test(str);
+}
+
+export function validatePhoneNumber(str){
+  return /^[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[\-\s]?[0-9]{4,6}$/im.test(str);
+}
---
+++ src/ch-14.test.js
@@ -0,0 +1,66 @@
+import { 
+  replaceZeros,
+  validatePin,
+  validateWord,
+  hasNumber,
+  validateEmail, 
+  validatePhoneNumber} from './ch-14.js';
+
+describe('Testing challenge 1', () => {
+  test('It should replace 0\'s with zero', () => {
+    expect(replaceZeros('1230450')).toStrictEqual('123zero45zero');
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should return true if pin is 4 digits long', () => {
+    expect(validatePin('1234')).toStrictEqual(true);
+    expect(validatePin('123')).toStrictEqual(false);
+    expect(validatePin('12345')).toStrictEqual(false);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should return true if word is between 5-10 digits long', () => {
+    expect(validateWord('abcd')).toStrictEqual(false);
+    expect(validateWord('abcde')).toStrictEqual(true);
+    expect(validateWord('abcdefghij')).toStrictEqual(true);
+    expect(validateWord('abcdefghijk')).toStrictEqual(false);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should return true if string has one or more letter followed by one or more digit', () => {
+    expect(hasNumber('abcd')).toStrictEqual(false);
+    expect(hasNumber('ab12')).toStrictEqual(true);
+    expect(hasNumber('a1')).toStrictEqual(true);
+    expect(hasNumber('12')).toStrictEqual(false);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should validate an email', () => {
+    expect(validateEmail('joe.joe@joe.com')).toStrictEqual(true);
+    expect(validateEmail('joe12@joe12.com')).toStrictEqual(true);
+    expect(validateEmail('joe@joe')).toStrictEqual(false);
+    expect(validateEmail('joe')).toStrictEqual(false);
+    expect(validateEmail('joe@joe.net')).toStrictEqual(true);
+    expect(validateEmail('joe@joe.org')).toStrictEqual(true);
+    expect(validateEmail('joe@joe.llama')).toStrictEqual(false);
+    expect(validateEmail('joe#$(@joe.com')).toStrictEqual(false);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should validate a phone number', () => {
+    expect(validatePhoneNumber('(555) 555-5555')).toStrictEqual(true);
+    expect(validatePhoneNumber('(555)555 5555')).toStrictEqual(true);
+    expect(validatePhoneNumber('555 555-5555')).toStrictEqual(false);
+    expect(validatePhoneNumber('555-5555555')).toStrictEqual(false);
+    expect(validatePhoneNumber('555-555 5555')).toStrictEqual(true);
+    expect(validatePhoneNumber('555-555-5555')).toStrictEqual(true);
+    expect(validatePhoneNumber('555 555 5555')).toStrictEqual(false);
+    expect(validatePhoneNumber('555555-5555')).toStrictEqual(false);
+    expect(validatePhoneNumber('5555555555')).toStrictEqual(false);
+  });
+});
